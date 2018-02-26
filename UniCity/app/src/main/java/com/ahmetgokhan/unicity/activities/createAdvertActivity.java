9a8e39e487301df83d372a151cb4fe95afd3f35d
gokhanklc <gokhankilic@Gokhans-MBP.mynet>
2018-02-27 00:06:04 +0300

    CreateAdvert was fixed and added new value named number of persons

--- UniCity/app/src/main/java/com/ahmetgokhan/unicity/activities/createAdvertActivity.java
+++ UniCity/app/src/main/java/com/ahmetgokhan/unicity/activities/createAdvertActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ahmetgokhan.unicity.R;
 import com.ahmetgokhan.unicity.overridden.UniSocial;
@@ -18,7 +19,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class createAdvertActivity extends AppCompatActivity {
-    EditText advertName,description,university,name,surname;
+    EditText advertName,description,university,name,numbOfPerson;
     Button createAdvertButton;
 
 
@@ -33,6 +34,7 @@ public class createAdvertActivity extends AppCompatActivity {
         university = findViewById(R.id.university);
         name = findViewById(R.id.name);
         createAdvertButton = findViewById(R.id.createAdvertButton);
+        numbOfPerson = findViewById(R.id.numbOfPerson);
 
 
         createAdvertButton.setOnClickListener(new View.OnClickListener() {
@@ -44,20 +46,50 @@ public class createAdvertActivity extends AppCompatActivity {
                 String description_text = description.getText().toString().trim();
                 String university_text = university.getText().toString().trim();
                 String name_text = name.getText().toString().trim();
+                int numberOfPerson = 0;
+                try {
+                    numberOfPerson = Integer.parseInt(numbOfPerson.getText().toString());
+                }catch (Exception e){};
 
 
-                Call<ArrayList<UniSocial>> call = apiInterface3.createAdvert(advertName_text,description_text,university_text,name_text);
-                call.enqueue(new Callback<ArrayList<UniSocial>>() {
-                    @Override
-                    public void onResponse(Call<ArrayList<UniSocial>> call, Response<ArrayList<UniSocial>> response) {
-                        System.out.println(response.body());
-                    }
 
-                    @Override
-                    public void onFailure(Call<ArrayList<UniSocial>> call, Throwable t) {
 
-                    }
-                });
+
+
+                if(advertName_text.isEmpty()){
+
+                    Toast.makeText(createAdvertActivity.this, "Please enter a advert name", Toast.LENGTH_SHORT).show();
+
+                }
+                else if(description_text.isEmpty()){
+
+                    Toast.makeText(createAdvertActivity.this, "Please enter a description", Toast.LENGTH_SHORT).show();
+
+                }
+
+                else if(String.valueOf(numberOfPerson).isEmpty() || numberOfPerson == 0){
+
+                    Toast.makeText(createAdvertActivity.this, "Please enter number of person", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+                else {
+
+
+                    Call<ArrayList<UniSocial>> call = apiInterface3.createAdvert(advertName_text, description_text, university_text, name_text, numberOfPerson);
+                    call.enqueue(new Callback<ArrayList<UniSocial>>() {
+                        @Override
+                        public void onResponse(Call<ArrayList<UniSocial>> call, Response<ArrayList<UniSocial>> response) {
+                           
+                        }
+
+                        @Override
+                        public void onFailure(Call<ArrayList<UniSocial>> call, Throwable t) {
+
+                        }
+                    });
+                }
             }
         });
 
