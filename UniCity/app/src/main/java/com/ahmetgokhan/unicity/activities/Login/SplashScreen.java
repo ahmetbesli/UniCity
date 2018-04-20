75dda5db8bd9dfa701e5f70c655a11e12cbf18d3
Ahmet Aziz Beşli <ahmet.besli@hotmail.com>
2018-04-20 04:35:34 +0300

    fixing

--- UniCity/app/src/main/java/com/ahmetgokhan/unicity/activities/Login/SplashScreen.java
+++ UniCity/app/src/main/java/com/ahmetgokhan/unicity/activities/Login/SplashScreen.java
@@ -7,6 +7,7 @@ import android.os.ConditionVariable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.ahmetgokhan.unicity.R;
 import com.ahmetgokhan.unicity.activities.Homepage.HomeActivity;
@@ -24,7 +25,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
+                
                 if (getApplicationContext().getSharedPreferences(Config.APP_NAME, MODE_PRIVATE).getBoolean(Config.LOGGING_STATUS, false)) {
                     if (getApplicationContext().getSharedPreferences(Config.APP_NAME, MODE_PRIVATE).getBoolean(Config.PROFILE_STATUS, false)) {
                         Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
@@ -37,8 +38,6 @@ public class SplashScreen extends AppCompatActivity {
                     Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivity(intent);
                 }
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
             }
         }, 2000);
     }
